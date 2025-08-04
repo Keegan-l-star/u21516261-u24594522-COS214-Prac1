2a56e879b7dbec51aa442a00928f6bc8680322db
Byron Pillay <byronpillay2704@gmail.com>
2025-08-04 15:59:21 +0200

    Update ShapeFactory.h

--- ShapeFactory.h
+++ ShapeFactory.h
@@ -1,1 +1,19 @@
+#ifndef SHAPEFACTORY_H
+#define SHAPEFACTORY_H
+
+class Shape;
+
+class ShapeFactory
+{
+protected:
+    virtual Shape *createShape() = 0;
+    virtual void toString() = 0;
+
+public:
+    ShapeFactory();
+    virtual ~ShapeFactory();
+};
+
+#endif
+
 
