04cecbc0422ae6b2d5710a4fccb55965a6c91b52
Byron Pillay <byronpillay2704@gmail.com>
2025-08-06 10:46:57 +0200

    Update TextboxFactory.cpp

--- TextboxFactory.cpp
+++ TextboxFactory.cpp
@@ -1,8 +1,9 @@
 #include "TextboxFactory.h"
 
 Shape* TextboxFactory::createShape(){
+    toString();
     return new TextBox();
 }
 void TextboxFactory::toString(){
-
+    std::cout<<"Textbox factory producing square."<<std::endl;
 }
