f362f68a2b4e32b7ac4dcfab97bca1ab68e0cbc5
Byron Pillay <byronpillay2704@gmail.com>
2025-08-09 11:54:31 +0200

    Add files via upload

--- TestingMain.cpp
+++ TestingMain.cpp
@@ -1,8 +1,93 @@
-#include "Canvas.h"
+#include "PDFExporter.h"
+#include "PNGExporter.h"
 
-int main(){
-    Canvas mspaint= Canvas();
+int main()
+{
+    // =====================================================================================================================================================================
+    // make and store a green rectangle
+    std::string type = "Rectangle";
+    int length = 9;
+    int width = 10;
+    int x = 20;
+    int y = 32;
+    std::string colour = "Green";
+
+    std::cout << "Creating canvas..." << std::endl;
+    Canvas *mspaint = new Canvas();
+
+    std::cout << "Creating " << type << "..." << std::endl;
+    mspaint->addShape(type);
+    std::cout << "Changing length to " << length << " and width to " << width << "..." << std::endl;
+    mspaint->changeLength(length);
+    mspaint->changeWidth(width);
+    std::cout << "Changing x co-ordinate to " << x << " and y co-ordinate to " << y << "..." << std::endl;
+    mspaint->changePosition(x, y);
+    std::cout<<"Changing colour to "<<colour<<"..."<<std::endl;
+    mspaint->changeColour(colour);
+    std::cout<<"Creating and storing a memento of "<<type<<" canvas element..."<<std::endl;
+    Memento *_memento_rec = mspaint->captureCurrent();
+    // mspaint->addToCanvas();
+    // =====================================================================================================================================================================
+
+
+    // =====================================================================================================================================================================
+    // clear canvas, make a red square and store it
+    type = "Square";
+    length = 15;
+    width = 15;
+    x = 30;
+    y = 29;
+    colour = "Red";
+
+    std::cout << "Clearing canvas..." << std::endl;
+    mspaint->clearCanvas();
+    std::cout << "Creating " << type << "..." << std::endl;
+    mspaint->addShape(type);
+    std::cout << "Changing length to " << length << " and width to " << width << "..." << std::endl;
+    mspaint->changeLength(length);
+    mspaint->changeWidth(width);
+    std::cout << "Changing x co-ordinate to " << x << " and y co-ordinate to " << y << "..." << std::endl;
+    mspaint->changePosition(x, y);
+    std::cout<<"Changing colour to "<<colour<<"..."<<std::endl;
+    mspaint->changeColour(colour);
+    std::cout<<"Creating and storing a memento of "<<type<<" canvas element..."<<std::endl;
+    Memento *_memento_square = mspaint->captureCurrent();
+    // =====================================================================================================================================================================
     
+    
+    // =====================================================================================================================================================================
+    // clear canvas, make a white textbox and store it
+    type = "Textbox";
+    length = 17;
+    width = 19;
+    x = 7;
+    y = 10;
+    colour = "White";
+
+    std::cout << "Clearing canvas..." << std::endl;
+    mspaint->clearCanvas();
+    std::cout << "Creating " << type << "..." << std::endl;
+    mspaint->addShape(type);
+    std::cout << "Changing length to " << length << " and width to " << width << "..." << std::endl;
+    mspaint->changeLength(length);
+    mspaint->changeWidth(width);
+    std::cout << "Changing x co-ordinate to " << x << " and y co-ordinate to " << y << "..." << std::endl;
+    mspaint->changePosition(x, y);
+    std::cout<<"Changing colour to "<<colour<<"..."<<std::endl;
+    mspaint->changeColour(colour);
+    std::cout<<"Creating and storing a memento of "<<type<<" canvas element..."<<std::endl;
+    Memento *_memento_txt = mspaint->captureCurrent();
+    // =====================================================================================================================================================================
+    //Export to PDF and PNG
+
+    std::cout<<"Attempting to export to PDF..."<<std::endl;
+    PDFExporter *_pdf = new PDFExporter(mspaint);
+    std::cout<<"Attempting to export to PNG..."<<std::endl;
+    PNGExporter *_png = new PNGExporter(mspaint);
+
+    _pdf->exportToFile();
+    _png->exportToFile();
+    // =====================================================================================================================================================================
+
     return 0;
 }
-
